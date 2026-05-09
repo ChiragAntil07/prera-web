513921463e1bf979e93e96e6a59dcef4a075fe86
Aryan-sangwan-29 <aryan@Aryan.local>
2026-05-09 11:58:35 +0530

    partners page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,34 @@
-import {Link} from 'react-router-dom';
-import './Navbar.css'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.css';
 
-export default function Nav(){
+export default function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div id='nav-div'>
+        <nav id='nav-div'>
+            <div className='nav-logo'>
+                <h2 style={{ color: 'white', margin: 0 }}>PRERA</h2>
+            </div>
 
-                <h2 style={{color:'white'}}>PRERA</h2>
+            {/* Hamburger Icon */}
+            <div className='menu-icon' onClick={toggleMenu}>
+                {isOpen ? <FaTimes color="white" size={24} /> : <FaBars color="white" size={24} />}
+            </div>
 
-
-                <div style={{display:'flex',gap:'30px',alignItems:'center'}}>
-                    <Link to='/' className='links'>Home</Link>
-                    <Link to='/about' className='links'>About</Link>
-                    <Link to='/events' className='links'>Events</Link>
-                    <Link to='/partner' className='links'>Partners</Link>
-                    <Link to='/connect' id='connect'>Lets Connect</Link>
-                </div>
-        </div>
-    )
+            {/* Nav Links */}
+            <div className={`nav-links ${isOpen ? 'active' : ''}`}>
+                <Link to='/' className='links' onClick={() => setIsOpen(false)}>Home</Link>
+                <Link to='/about' className='links' onClick={() => setIsOpen(false)}>About</Link>
+                <Link to='/events' className='links' onClick={() => setIsOpen(false)}>Events</Link>
+                <Link to='/partner' className='links' onClick={() => setIsOpen(false)}>Partners</Link>
+                <Link to='/connect' id='connect' onClick={() => setIsOpen(false)}>Lets Connect</Link>
+            </div>
+        </nav>
+    );
 }
--- src/Partners_compoonents/partners.jsx
+++ src/Partners_compoonents/partners.jsx
@@ -1,8 +1,18 @@
 import './Partners.css';
 import React from "react";
 import Nav from '../Navbar.jsx';
-import logo1 from '../assets/Anindya.png'; // DO NOT DELETE THESE IMPORTS
-import logo2 from '../assets/Culmyca.png'; // THEY ARE NEEDED FOR THE LOGOS BELOW
+import redbull from '../assets/partners/redbill.png';
+import canva from '../assets/partners/canva.png';
+import axure from '../assets/partners/axure.png';
+import denver from '../assets/partners/denver.png';
+import dell from '../assets/partners/dell.png';
+import truescholar from '../assets/partners/truescholar.png';
+import patym from '../assets/partners/patym.png';
+import hell from '../assets/partners/hell.png';
+import abhibus from '../assets/partners/abhibus.png';
+import unstop from '../assets/partners/unstop.png';
+import dominos from '../assets/partners/dominos.png';
+import unibazar from '../assets/partners/unibazar.png';
 
 
 function Partners() {
@@ -10,40 +20,28 @@ function Partners() {
     <div className="partnerPage">
 
       <div className="partnerTop">
-
         <h1 className="mainHeading">
           OUR <span>PARTNERS</span>
         </h1>
-
         <p className="subText">
           We collaborate with leading brands to create impactful event
           experiences and meaningful partnerships.
         </p>
       </div>
-
       <div className="logoGrid">
-        <div className="logoBox"><img src={logo1} alt="logo 1" /></div>
-        <div className="logoBox"><img src={logo2} alt="logo 2" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 3" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 4" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 5" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 6" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 7" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 8" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 9" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 10" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 11" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 12" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 13" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 14" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 15" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 16" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 17" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 18" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 19" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 20" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 21" /></div>
-        <div className="logoBox"><img src={logo1} alt="logo 22" /></div>
+        <div className="logoBox"><img src={redbull} alt="Red Bull" /></div>
+        <div className="logoBox"><img src={canva} alt="Canva" /></div>
+        <div className="logoBox"><img src={axure} alt="Axure" /></div>
+        <div className="logoBox"><img src={denver} alt="Denver" /></div>
+        <div className="logoBox"><img src={dell} alt="Dell" /></div>
+        <div className="logoBox"><img src={truescholar} alt="TrueScholar" /></div>
+        <div className="logoBox"><img src={patym} alt="Patym" /></div>
+        <div className="logoBox"><img src={hell} alt="Hell" /></div>
+        <div className="logoBox"><img src={abhibus} alt="Abhibus" /></div>
+        <div className="logoBox"><img src={unstop} alt="Unstop" /></div>
+        <div className="logoBox"><img src={dominos} alt="Dominos" /></div>
+        <div className="logoBox"><img src={unibazar} alt="Unibazar" /></div>
+
       </div>
 
     </div>
